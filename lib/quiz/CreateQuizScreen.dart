e92f18bfe2c88a75775feea44e7c3c88687dbb72
VietPQ <vietpq@ikameglobal.com>
2024-04-19 23:55:53 +0700

    add api posting

--- lib/quiz/CreateQuizScreen.dart
+++ lib/quiz/CreateQuizScreen.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 
 
@@ -49,7 +51,28 @@ class _CreateQuizScreenState extends State<CreateQuizScreen> {
             const SizedBox(height: 16.0),
             ElevatedButton(
               onPressed: () {
-                // Xử lý khi nhấn nút Submit
+                createQuiz(
+                  name: 'string',
+                  description: 'string',
+                  startTime: '2024-04-19T16:09:54.111Z',
+                  duration: '00:00:00',
+                  users: ['string'],
+                  questions: [
+                    {
+                      'questionText': 'string',
+                      'choices': [
+                        {
+                          'choiceText': 'string',
+                          'correct': true,
+                          'order': 0,
+                        },
+                      ],
+                      'order': 0,
+                      'quizId': 0,
+                      'multipleChoice': true,
+                    },
+                  ],
+                );
                 setState(() {
                   qrData = 'Dữ liệu của bạn';
                 });
@@ -83,5 +106,42 @@ class _CreateQuizScreenState extends State<CreateQuizScreen> {
         ),
       ),
     );
+  }
+}
+
+Future<void> createQuiz({
+  required String name,
+  required String description,
+  required String startTime,
+  required String duration,
+  required List<String> users,
+  required List<Map<String, dynamic>> questions,
+}) async {
+  final String apiUrl = 'http://35.197.148.25:8080/api/v1/create-quiz/1';
+
+  final response = await http.post(
+    Uri.parse(apiUrl),
+    headers: <String, String>{
+      'accept': '*/*',
+      'username': 'string',
+      'role': 'TEACHER',
+      'Content-Type': 'application/json',
+    },
+    body: jsonEncode(<String, dynamic>{
+      'name': name,
+      'description': description,
+      'config': {
+        'startTime': startTime,
+        'duration': duration,
+      },
+      'users': users,
+      'questions': questions,
+    }),
+  );
+
+  if (response.statusCode == 200) {
+    print('Quiz created successfully');
+  } else {
+    print('Failed to create quiz. Status code: ${response.statusCode}');
   }
 }
