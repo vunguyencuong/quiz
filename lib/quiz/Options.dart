ab078f49e780ee85f39453c7cd594943468c99f9
ACER <vucuong05052002@gmail.com>
2024-04-21 23:33:36 +0700

    join quiz: deselection, save answers

--- lib/quiz/Options.dart
+++ lib/quiz/Options.dart
@@ -8,18 +8,26 @@ import 'QuizScreen.dart';
 class Options extends StatelessWidget {
   final Choice options;
   final QuizController controller = Get.find();
-
-  Options({Key? key, required this.options}) : super(key: key);
+  late RxBool isSelected;
+  Options({Key? key, required this.options}) : super(key: key){
+    isSelected = RxBool(controller.selectedAnswers.contains(options));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Obx(() {
-      final isSelected = controller.selectedAnswers.contains(options);
+      isSelected.value = controller.selectedAnswers.contains(options);
       return Column(
         children: [
           GestureDetector(
             onTap: () {
-              controller.selectAnswer(options);
+              if (isSelected.value) {
+                isSelected.value = !isSelected.value;
+                controller.unSelectAnswer(options);
+              } else {
+                isSelected.value = true;
+                controller.selectAnswer(options);
+              }
             },
             child: Container(
               height: 48,
@@ -29,9 +37,9 @@ class Options extends StatelessWidget {
                 borderRadius: BorderRadius.circular(15),
                 border: Border.all(
                   width: 3,
-                  color: isSelected ? Color(0xff90CAF9) : Color(0xff90CAF9),
+                  color: isSelected.value ? Color(0xff90CAF9) : Color(0xff90CAF9),
                 ),
-                color: isSelected ? Color(0xFFBBDEFB) : Colors.transparent,
+                color: isSelected.value ? Color(0xFFBBDEFB) : Colors.transparent,
               ),
               child: Padding(
                 padding: EdgeInsets.symmetric(horizontal: 18),
